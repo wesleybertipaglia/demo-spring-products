f0e52d2982943b8ec0ce35d0a27aba72c720c87f
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-24 01:03:32 -0300

    feat: add filter and pagination to ProductController

--- src/main/java/com/wesleybertipaglia/products/controllers/ProductController.java
+++ src/main/java/com/wesleybertipaglia/products/controllers/ProductController.java
@@ -1,93 +1,90 @@
 package com.wesleybertipaglia.products.controllers;
 
+import com.wesleybertipaglia.products.dtos.ProductRecordDto;
+import com.wesleybertipaglia.products.models.ProductModel;
+import com.wesleybertipaglia.products.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wesleybertipaglia.products.dtos.ProductRecordDto;
-import com.wesleybertipaglia.products.models.ProductModel;
-import com.wesleybertipaglia.products.repositories.ProductRepository;
-
-import jakarta.validation.Valid;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 
     @Autowired
-    ProductRepository productRepository;
+    private ProductRepository productRepository;
 
-    @PostMapping("/products")
+    @PostMapping
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto) {
-        var productModel = new ProductModel();
+        ProductModel productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
     }
 
-    @GetMapping("/products")
-    public ResponseEntity<List<ProductModel>> getAllProducts() {
-        List<ProductModel> productList = productRepository.findAll();
-
-        if (!productList.isEmpty()) {
-            for (ProductModel productModel : productList) {
-                productModel.add(linkTo(methodOn(ProductController.class).getOneProduct(
-                        productModel.getId())).withSelfRel());
-            }
-        }
-
+    @GetMapping
+    public ResponseEntity<List<ProductModel>> getAllProducts(
+            @RequestParam(required = false) String filter,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        List<ProductModel> productList = findProducts(filter, page, size);
+        addLinksToProducts(productList);
         return ResponseEntity.status(HttpStatus.OK).body(productList);
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID id) {
         Optional<ProductModel> product = productRepository.findById(id);
-
         if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
-
-        product.get().add(linkTo(methodOn(ProductController.class).getAllProducts()).withRel("Products List"));
+        product.get().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ProductController.class)
+                .getAllProducts(null, 0, 10)).withRel("Products List"));
         return ResponseEntity.ok().body(product.get());
     }
 
-    @PutMapping("/products/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Object> updateProduct(@PathVariable(value = "id") UUID id,
             @RequestBody @Valid ProductRecordDto productRecordDto) {
         Optional<ProductModel> product = productRepository.findById(id);
-
         if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
-
         var productModel = product.get();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.ok().body(productRepository.save(productModel));
     }
 
-    @DeleteMapping("/products/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
         Optional<ProductModel> product = productRepository.findById(id);
-
         if (product.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
         }
-
         productRepository.delete(product.get());
         return ResponseEntity.ok().body("Product deleted successfully.");
+    }
+
+    private List<ProductModel> findProducts(String filter, int page, int size) {
+        if (filter != null && !filter.isEmpty()) {
+            return productRepository.findByNameContainingIgnoreCase(filter);
+        } else {
+            return productRepository.findAll(PageRequest.of(page, size)).getContent();
+        }
+    }
+
+    private void addLinksToProducts(List<ProductModel> productList) {
+        for (ProductModel productModel : productList) {
+            productModel.add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(ProductController.class)
+                    .getOneProduct(productModel.getId())).withSelfRel());
+        }
     }
 }
--- src/main/java/com/wesleybertipaglia/products/repositories/ProductRepository.java
+++ src/main/java/com/wesleybertipaglia/products/repositories/ProductRepository.java
@@ -1,12 +1,13 @@
 package com.wesleybertipaglia.products.repositories;
 
-import java.util.UUID;
-
+import com.wesleybertipaglia.products.models.ProductModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.wesleybertipaglia.products.models.ProductModel;
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+    List<ProductModel> findByNameContainingIgnoreCase(String name);
 }
