8ef07c9883bf52badeaae04b3782373cc6cbfa4a
wesleybertipaglia <wesleybertipaglia@gmail.com>
2024-06-23 23:23:14 -0300

    feat: add deleteProduct to ProductController

--- src/main/java/com/wesleybertipaglia/products/controllers/ProductController.java
+++ src/main/java/com/wesleybertipaglia/products/controllers/ProductController.java
@@ -19,6 +19,7 @@ import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -64,5 +65,17 @@ public class ProductController {
         var productModel = product.get();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.ok().body(productRepository.save(productModel));
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable(value = "id") UUID id) {
+        Optional<ProductModel> product = productRepository.findById(id);
+
+        if (product.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
+
+        productRepository.delete(product.get());
+        return ResponseEntity.ok().body("Product deleted successfully.");
     }
 }
